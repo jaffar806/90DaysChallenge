4936f7694cb81967fff86a784335f4332fe059e5
Md Jaffar Hussain <146517898+jaffar806@users.noreply.github.com>
2023-11-03 15:25:26 +0530

    Update Pattern78.java

--- day7/Pattern78.java
+++ day7/Pattern78.java
@@ -1,7 +1,15 @@
 package day7;
 
 public class Pattern78 {
+/*
+15 14 13 12 11 
+10 9 8 7 
+6 5 4 
+3 2 
+1 
 
+
+*/
 	public Pattern78(int n) {
 		int x= (n*(n+1))/2; //sum of the n terms
 		for(int row=n; row>=0; row--) {
