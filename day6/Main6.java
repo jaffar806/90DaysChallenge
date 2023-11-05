d68f1e2bc3294f456194dd74074f3dfedd3080f9
Md Jaffar Hussain <146517898+jaffar806@users.noreply.github.com>
2023-11-05 23:10:20 +0530

    Add files via upload

---
+++ day6/Main6.java
@@ -0,0 +1,30 @@
+package day6;
+
+import java.util.Scanner;
+
+public class Main6 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("enter the size of the row you want:");
+		Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+        //Pattern34 call = new Pattern34(n);
+        //Pattern35 call = new Pattern35(n);
+        //Pattern36 call = new Pattern36(n);
+        //Pattern37 call = new Pattern37(n);
+        //Pattern38 call = new Pattern38(n);
+        //Pattern39 call = new Pattern39(n);
+        //Pattern40 call = new Pattern40(n);
+        //Pattern41 call = new Pattern41(n);
+        //Pattern42 call = new Pattern42(n);
+        //Pattern43 call = new Pattern43(n);
+        //Pattern44 call = new Pattern44(n);
+        //Pattern45 call = new Pattern45(n);
+        //Pattern46 call = new Pattern46(n);
+        Pattern47 call = new Pattern47(n);
+        
+	}
+
+}
