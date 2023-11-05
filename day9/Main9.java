bea745ae0421e78428b45611552375b797686308
Md Jaffar Hussain <146517898+jaffar806@users.noreply.github.com>
2023-11-05 23:06:12 +0530

    Add files via upload

---
+++ day9/Main9.java
@@ -0,0 +1,33 @@
+package day9;
+
+import java.util.Scanner;
+
+public class Main9 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the size of the row: ");
+		Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+        //Pattern107 call = new Pattern107(n);
+        //Pattern108 call = new Pattern108(n);
+        //Pattern112 call = new Pattern112(n);
+        //Pattern113 call = new Pattern113(n);
+        //Pattern114 call = new Pattern114(n);
+        //Pattern115 call = new Pattern115(n);  	
+        //Pattern116 call = new Pattern116(n);
+        //Pattern117 call = new Pattern117(n);
+        //Pattern118 call = new Pattern118(n);
+        //Pattern119 call = new Pattern119(n);
+        //Pattern120 call = new Pattern120(n);
+        //Pattern121 call = new Pattern121(n);
+        //Pattern122 call = new Pattern122(n);
+        //Patternb call = new Patternb(n);
+        //Pattern123 call = new Pattern123(n);
+        //Pattern124 call = new Pattern124(n);
+        //Pattern125 call = new Pattern125(n);  
+        //Pattern126 call = new Pattern126(n);
+        Pattern127 call = new Pattern127(n);	
+	}
+
+}
