7ff9ab06d0f39fce1c66ce1d00a16c6e3b2c17dc
Md Jaffar Hussain <146517898+jaffar806@users.noreply.github.com>
2023-11-05 23:09:30 +0530

    Add files via upload

---
+++ day7/Main7.java
@@ -0,0 +1,49 @@
+package day7;
+
+import java.util.Scanner;
+
+public class Main7 {
+
+	public static void main(String[] args) {
+		System.out.print("enter the size of the row you want:");
+		Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+        //Pattern59 call = new Pattern59(n);
+        //Pattern60 call = new Pattern60(n);
+        //Pattern61 call = new Pattern61(n);
+        //Pattern62 call = new Pattern62(n);
+        //Pattern63 call = new Pattern63(n);
+        //Pattern64 call = new Pattern64(n);
+        //Pattern65 call = new Pattern65(n);
+        //Pattern66 call = new Pattern66(n);
+        //Pattern67 call = new Pattern67(n);
+        //Pattern68 call = new Pattern68(n);
+        //Pattern69 call = new Pattern69(n);
+        //Pattern70 call = new Pattern70(n); //*** problems
+        //Pattern71 call = new Pattern71(n); 
+        //Pattern72 call = new Pattern72(n);
+        //Pattern73 call = new Pattern73(n);
+        //Pattern74 call = new Pattern74(n);
+        //Pattern75 call = new Pattern75(n);
+        //Pattern76 call = new Pattern76(n);
+        //Pattern77 call = new Pattern77(n);
+        //Pattern78 call = new Pattern78(n);
+        //Pattern79 call = new Pattern79(n);
+        //Pattern81 call = new Pattern81(n);
+        //Pattern82 call = new Pattern82(n);
+        //Pattern83 call = new Pattern83(n);
+        //Pattern84 call = new Pattern84(n);
+        //Pattern85 call = new Pattern85(n);
+        //Pattern86 call = new Pattern86(n);
+        //Pattern87 call = new Pattern87(n);
+        //Pattern88 call = new Pattern88(n);
+        //Pattern89 call = new Pattern89(n);
+        //Pattern90 call = new Pattern90(n);
+       // Pattern91 call = new Pattern91(n);
+        
+	
+	
+	}
+
+}
