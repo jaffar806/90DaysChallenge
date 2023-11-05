dd2511df89d37f92aa432ddcf2efd9042894c1af
Md Jaffar Hussain <146517898+jaffar806@users.noreply.github.com>
2023-11-05 23:11:42 +0530

    Add files via upload

---
+++ day4/main4.java
@@ -0,0 +1,45 @@
+package day4;
+
+import java.util.Scanner;
+
+public class main4 {
+
+	public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter number of lines of you want: ");
+    
+    int num = input.nextInt();
+  //  char total =input.next().charAt(0);
+    
+    
+    
+   // Pattern5 first = new Pattern5(num);   //capital p is there first
+   // pattern6 first = new pattern6(num);     first letter of p is small
+   // Pattern7 first = new Pattern7(num);     
+    //Pattern8 first = new Pattern8(num);
+    //Pattern9 first = new Pattern9(num);
+    //Pattern10 first = new Pattern10(num);
+    Pattern11 first = new Pattern11(num);
+    //Pattern12 first = new Pattern12(num);
+    //Pattern13 first = new Pattern13(num);
+    //Pattern14 first = new Pattern14(num);
+    //Pattern15 first = new Pattern15(num);
+    //Pattern16 first = new Pattern16(num);
+    //Pattern17 first = new Pattern17(num);
+    //Pattern18 first = new Pattern18(num);
+    //Pattern19 first = new Pattern19(num);
+    //Pattern20 first = new Pattern20(num);
+    //Pattern21 first = new Pattern21(num);
+   // Pattern22 first = new Pattern22(num);
+    //Pattern23 first = new Pattern23(num);
+   // Pattern24 first = new Pattern24(num);
+    //Pattern25 first = new Pattern25(num);
+    //Pattern26 first = new Pattern26(total); charecter programs from here
+	//Pattern27 first = new Pattern27(total);
+    //Pattern28 first = new Pattern28(total);
+    //Pattern29 first = new Pattern29(total);
+    //Pattern30 first = new Pattern30(num);	
+	
+	}
+
+}
